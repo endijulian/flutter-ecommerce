c597711d1b15ba3401c96ccc753a82d6c9a6934b
endijulian <endijulian080798@gmail.com>
2022-10-15 01:26:00 +0700

    profile page, header fix

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(defaultMargin),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/image_profile.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Alex',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        '@alexkeinn',
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: reguler,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/btn_exit.png',
+                  width: 20,
+                  height: 20,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
