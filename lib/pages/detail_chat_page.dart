8504d7631d860a52807cfa99de8e55e3ece974fe
endijulian <endijulian080798@gmail.com>
2022-10-14 21:55:28 +0700

    detail chat page

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -45,40 +45,103 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
-    Widget chatInput() {
+    Widget productPriview() {
       return Container(
-        margin: EdgeInsets.all(20),
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(
+          bottom: 20,
+        ),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: primaryColor),
+        ),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                decoration: BoxDecoration(
-                  color: backgroundColor4,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: TextFormField(
-                    style: primaryTextStyle,
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Typle Message...',
-                      hintStyle: subtitleTextStyle,
-                      // hintStyle: Co,
-                    ),
-                  ),
-                ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
               ),
             ),
             SizedBox(
-              width: 20,
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
             ),
             Image.asset(
-              'assets/btn_send.png',
-              width: 45,
+              'assets/btn_close.png',
+              width: 22,
             )
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            productPriview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Typle Message...',
+                          hintStyle: subtitleTextStyle,
+                          // hintStyle: Co,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/btn_send.png',
+                  width: 45,
+                )
+              ],
+            ),
           ],
         ),
       );
