22554d7a3bb2e34f7b363130c650ab004d140104
endijulian <endijulian080798@gmail.com>
2022-10-16 03:03:26 +0700

    product page, header

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/pages/detail_chat_page.dart';
 import 'package:flutter_ecommerce/pages/edit_profile_page.dart';
 import 'package:flutter_ecommerce/pages/home/main_page.dart';
+import 'package:flutter_ecommerce/pages/product_page.dart';
 import 'package:flutter_ecommerce/pages/sign_in_page.dart';
 import 'package:flutter_ecommerce/pages/sign_up_page.dart';
 import 'package:flutter_ecommerce/pages/splash_page.dart';
@@ -24,6 +25,7 @@ class MyApp extends StatelessWidget {
         '/home': (context) => MainPage(),
         '/detail-chat': (context) => DetailChatPage(),
         '/edit-profile': (context) => EditProfilePage(),
+        '/product': (context) => ProductPage(),
       },
     );
   }
