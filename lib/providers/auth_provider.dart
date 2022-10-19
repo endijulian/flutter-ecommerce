b4843c2123577fc16d94281287ca1a10819c259d
endijulian <endijulian080798@gmail.com>
2022-10-19 21:11:13 +0700

    models, providers, and services user

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/models/users_model.dart';
+import 'package:flutter_ecommerce/services/auth_service.dart';
+
+class AuthProvider with ChangeNotifier {
+  late UserModel _user;
+
+  UserModel get user => _user;
+
+  set user(UserModel user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  Future<bool> register({
+    String? name,
+    String? username,
+    String? email,
+    String? password,
+  }) async {
+    try {
+      UserModel user = await AuthService().register(
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+      );
+
+      _user = user;
+      return true;
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+      return false;
+    }
+  }
+}
